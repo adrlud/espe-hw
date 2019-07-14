ded7f6bac3b17fc2f215eaee4ce62b0eacd3e970
Maximilian Ludvigsson <max@textual.se>
2019-07-14 19:22:29 +0200

    Create JSON on the fly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "Arduino.h"
 #include "HX711.h"
 
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
 // Constants
 const int ledPin = LED_BUILTIN;
 const long interval = 1000;
+const long httpInterval = 500;
+
+const String DEVICES_PATH = "http://192.168.0.9:8000/devices";
 
 // Variables
 int ledState = LOW;
@@ -15,20 +22,22 @@ HX711 loadcell;
 const int LOADCELL_DOUT_PIN = D6;
 const int LOADCELL_SCK_PIN = D5;
 
-float calibration_factor = 250; 
+float calibration_factor = 25; 
 
 void setup () {
     pinMode(ledPin, OUTPUT);
     Serial.begin(9600);
-    Serial.println("Hello");
 
     loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    loadcell.set_scale();
+    loadcell.set_scale(calibration_factor); //Adjust to this calibration factor
     loadcell.tare();
 
-    long zero_factor = loadcell.read_average();
-    Serial.print("Zero factor: ");
-    Serial.println(zero_factor);
+    WiFi.begin("comhem_D4E240", "7f8c6pvt");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Waiting for connection...");
+    }
 }
 
 int switchLedState(int current) {
@@ -39,30 +48,72 @@ int switchLedState(int current) {
     }
 }
 
-void loop () {
+void createMeasurement(String& output, long reading) {
+    const size_t capacity = JSON_OBJECT_SIZE(2);
+    DynamicJsonDocument doc(capacity);
 
+    doc["device_id"] = 1;
+    doc["reading"] = reading;
+    serializeJson(doc, output);
+}
+
+void loop () {
     unsigned long currentMillis = millis();
     
-    if (currentMillis - previousMillis >= interval) {
-        ledState = switchLedState(ledState);
-        digitalWrite(ledPin, ledState);
+
+    // ----------------------
+    // HTTP
+    // ----------------------
+    if (currentMillis - previousMillis >= httpInterval) {
+        String output;
+        float reading = loadcell.get_units(10);
+        createMeasurement(output, reading);
+        Serial.println(output);
+
+        Serial.println("Starting http request");
+        if (false) {
+        // if (WiFi.status() == WL_CONNECTED) {
+            WiFiClient client;
+            HTTPClient http;
+            http.begin(client, DEVICES_PATH);
+            int httpCode = http.GET();
+
+            if (httpCode > 0) {
+                String payload = http.getString();
+                Serial.println(payload);
+            } else {
+                Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            }
+
+            http.end();
+        }
         previousMillis = currentMillis;
     }
-    loadcell.set_scale(calibration_factor); //Adjust to this calibration factor
 
-    Serial.print("Reading: ");
-    Serial.print(loadcell.get_units(), 1);
-    Serial.print(" kg");
-    Serial.print(" calibration_factor: ");
-    Serial.print(calibration_factor);
-    Serial.println();
+    // ----------------------
+    // Readings
+    // ----------------------
+    
+    // if (currentMillis - previousMillis >= interval) {
+    //     ledState = switchLedState(ledState);
+    //     digitalWrite(ledPin, ledState);
+    //     previousMillis = currentMillis;
+    // }
+    // loadcell.set_scale(calibration_factor); //Adjust to this calibration factor
 
-    if(Serial.available())
-    {
-        char temp = Serial.read();
-        if(temp == '+' || temp == 'a')
-        calibration_factor += 10;
-        else if(temp == '-' || temp == 'z')
-        calibration_factor -= 10;
-    }
+    // Serial.print("Reading: ");
+    // Serial.print(loadcell.get_units(), 1);
+    // Serial.print(" kg");
+    // Serial.print(" calibration_factor: ");
+    // Serial.print(calibration_factor);
+    // Serial.println();
+
+    // if(Serial.available())
+    // {
+    //     char temp = Serial.read();
+    //     if(temp == '+' || temp == 'a')
+    //     calibration_factor += 10;
+    //     else if(temp == '-' || temp == 'z')
+    //     calibration_factor -= 10;
+    // }
 }
