a532d7842599082e6ba648a204ec83dabf6f211a
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2019-07-14 17:45:19 +0200

    Basic main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,68 @@
 #include "Arduino.h"
 #include "HX711.h"
 
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+// Constants
+const int ledPin = LED_BUILTIN;
+const long interval = 1000;
+
+// Variables
+int ledState = LOW;
+unsigned long previousMillis = 0;
+
+// HX711
+HX711 loadcell;
+
+const int LOADCELL_DOUT_PIN = D6;
+const int LOADCELL_SCK_PIN = D5;
+
+float calibration_factor = 250; 
+
+void setup () {
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("Hello");
+
+    loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    loadcell.set_scale();
+    loadcell.tare();
+
+    long zero_factor = loadcell.read_average();
+    Serial.print("Zero factor: ");
+    Serial.println(zero_factor);
 }
 
-void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+int switchLedState(int current) {
+    if (current == LOW) {
+        return HIGH;
+    } else {
+        return LOW;
+    }
+}
+
+void loop () {
+
+    unsigned long currentMillis = millis();
+    
+    if (currentMillis - previousMillis >= interval) {
+        ledState = switchLedState(ledState);
+        digitalWrite(ledPin, ledState);
+        previousMillis = currentMillis;
+    }
+    loadcell.set_scale(calibration_factor); //Adjust to this calibration factor
+
+    Serial.print("Reading: ");
+    Serial.print(loadcell.get_units(), 1);
+    Serial.print(" kg");
+    Serial.print(" calibration_factor: ");
+    Serial.print(calibration_factor);
+    Serial.println();
+
+    if(Serial.available())
+    {
+        char temp = Serial.read();
+        if(temp == '+' || temp == 'a')
+        calibration_factor += 10;
+        else if(temp == '-' || temp == 'z')
+        calibration_factor -= 10;
+    }
 }
